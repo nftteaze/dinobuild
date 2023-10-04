788daef34f6030da6944496a1d5c7019cae0dedc
nftteaze <t72915318@gmail.com>
2023-10-04 07:14:28 +0100

    slower, graphics updated

--- src/components/Dino/Dino.js
+++ src/components/Dino/Dino.js
@@ -2,16 +2,14 @@ import React, { useEffect, useRef, useState } from "react";
 import "./Dino.css";
 
 function Dino() {
-  //ref to get 'dino' html element in js
   const dinoRef = useRef();
-  //ref to get 'cactus' html element in js
   const cactusRef = useRef();
+  const groundRef = useRef(); // Ref for the ground element
+
   const [score, setScore] = useState(0);
 
-  //method to add 'jump' class every '300ms' as the class jump css has jumping animation of 0.3s(300ms).
-  //so on each key press we need to add animation and remove animation
   const jump = () => {
-    if (!!dinoRef.current && dinoRef.current.classList != "jump") {
+    if (!!dinoRef.current && !dinoRef.current.classList.contains("jump")) {
       dinoRef.current.classList.add("jump");
       setTimeout(function () {
         dinoRef.current.classList.remove("jump");
@@ -19,23 +17,17 @@ function Dino() {
     }
   };
 
-  //useEffect to track whether dino position and cactus position is intersecting
-  //if yes, then game over.
   useEffect(() => {
     const isAlive = setInterval(function () {
-      // get current dino Y position
       const dinoTop = parseInt(
         getComputedStyle(dinoRef.current).getPropertyValue("top")
       );
 
-      // get current cactus X position
       let cactusLeft = parseInt(
         getComputedStyle(cactusRef.current).getPropertyValue("left")
       );
 
-      // detect collision
       if (cactusLeft < 40 && cactusLeft > 0 && dinoTop >= 140) {
-        // collision
         alert("Game Over! Your Score : " + score);
         setScore(0);
       } else {
@@ -46,7 +38,6 @@ function Dino() {
     return () => clearInterval(isAlive);
   });
 
-  //hook to call jump method on any keypress
   useEffect(() => {
     document.addEventListener("keydown", jump);
     return () => document.removeEventListener("keydown", jump);
@@ -54,7 +45,8 @@ function Dino() {
 
   return (
     <div className="game">
-      Score : {score}
+      <div id="ground" ref={groundRef}></div>
+      Score: {score}
       <div id="dino" ref={dinoRef}></div>
       <div id="cactus" ref={cactusRef}></div>
     </div>
